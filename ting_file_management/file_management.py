071c1ecb4712570d70d626db38569cd04b429b4d
Mariana <mlobosilva@outlook.com>
2023-06-13 19:29:35 -0300

    feat: cria função para ler arquivos válidos

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido")
+    with open(path_file, mode="r") as file:
+        lines = file.read().split("\n")
+        return lines
