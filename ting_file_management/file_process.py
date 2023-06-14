94f1f159fecdc9af4f9629fba3f84e5f9cdc21a6
Mariana <mlobosilva@outlook.com>
2023-06-14 14:55:00 -0300

    feat: permite processar arquivos

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+# import os
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    # file_name = os.path.basename(path_file)
+    for i in instance._data:
+        if i["nome_do_arquivo"] == path_file:
+            return
+
+    file_lines = txt_importer(path_file)
+
+    file_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines),
+        "linhas_do_arquivo": file_lines
+    }
+
+    instance.enqueue(file_dict)
+    print(file_dict)
 
 
 def remove(instance):
