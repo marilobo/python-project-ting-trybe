4cca4182efcdae4311e524a846d6e26026c0ca90
Mariana <mlobosilva@outlook.com>
2023-06-15 09:21:55 -0300

    linter

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -5,7 +5,7 @@ def exists_word(word, instance):
         lines = []
         for index, line in enumerate(file["linhas_do_arquivo"]):
             if word_lower in line.lower():
-                lines.append({ "linha": index + 1 })
+                lines.append({"linha": index + 1})
         if lines:
             result.append({
                 "palavra": word,
@@ -14,6 +14,7 @@ def exists_word(word, instance):
             })
     return result
 
+
 def search_by_word(word, instance):
     word_lower = word.lower()
     result = []
@@ -21,8 +22,8 @@ def search_by_word(word, instance):
         info = []
         for index, line in enumerate(file["linhas_do_arquivo"]):
             if word_lower in line.lower():
-                info.append({ "linha": index + 1, "conteudo": line })
-                
+                info.append({"linha": index + 1, "conteudo": line})
+
         if info:
             result.append({
                 "palavra": word,
