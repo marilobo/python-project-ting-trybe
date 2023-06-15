3feabe5c15bc19ba31480f378fd84f0739817ab8
Mariana <mlobosilva@outlook.com>
2023-06-15 09:16:54 -0300

    feat: requisito 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -15,4 +15,18 @@ def exists_word(word, instance):
     return result
 
 def search_by_word(word, instance):
-    ...
+    word_lower = word.lower()
+    result = []
+    for file in instance._data:
+        info = []
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word_lower in line.lower():
+                info.append({ "linha": index + 1, "conteudo": line })
+                
+        if info:
+            result.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": info
+            })
+    return result
